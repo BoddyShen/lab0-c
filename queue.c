0b134cbc83eabb008db163b8b58c14377aecf2b5
Po-Heng Shen <f2213851026@gmail.com>
2025-05-22 04:48:49 -0400

    Implement q_new, q_free, q_insert_head, q_insert_tail
    Change-Id: I1e7225a0027e4f2e53924965cef124fd044ed9c7

--- queue.c
+++ queue.c
@@ -7,22 +7,78 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new_node = malloc(sizeof(struct list_head));
+    if (new_node == NULL)
+        return NULL;
+    INIT_LIST_HEAD(new_node);
+    return new_node;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (head == NULL)
+        return;
+
+    struct list_head *curr_node, *next_node;
+
+    curr_node = head->next;
+
+    while (curr_node->next != head) {
+        next_node = curr_node->next;
+        element_t *entry = list_entry(curr_node, element_t, list);
+        if (entry->value) {
+            free(entry->value);
+            entry->value = NULL;
+        }
+        free(curr_node);
+        curr_node = next_node;
+    }
+    free(head);
+}
+
+/* Insert an element at node's next position of queue */
+bool insert_next(struct list_head *node, char *s)
+{
+    if (node == NULL)
+        return false;
+
+    // element_t contains struct list_head, so no need to malloc to list_head
+    element_t *new_element = malloc(sizeof(element_t));
+    if (new_element == NULL)
+        return false;
+
+    if (s != NULL) {
+        size_t s_len = strlen(s) + 1;
+        new_element->value = malloc(s_len + 1);
+        if (new_element->value == NULL) {
+            free(new_element);
+            return false;
+        }
+        strncpy(new_element->value, s, s_len);
+    } else {
+        new_element->value = NULL;
+    }
+
+    list_add(&new_element->list, node);
+
+    return true;
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
-    return true;
+    return insert_next(head, s);
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    return true;
+    if (!head) {
+        return false;
+    }
+
+    return insert_next(head->prev, s);
 }
 
 /* Remove an element from head of queue */
